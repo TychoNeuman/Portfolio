5f36b7307a779e19c6c3b96bf9a792be60bd7716
Tycho Neuman <tycho.neuman@mijngeldzaken.nl>
2019-12-10 17:23:22 +0100

    progress

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
+//Loading screen
+$(document).ready(function(){
+  $('.loading').delay(1200).hide(0);
+  setTimeout(function(){
+    $('#bobbie').removeClass('hidden');
+  }, 1200);
+});
+// Animate the title test
 $('#title').text('Front-end')
   .delay(800)
   .slideDown(800)
@@ -8,3 +16,10 @@ $('#title').text('Front-end')
     .delay(800)
     .slideUp(800);
   });
+
+// Animate Bobbie on scroll
+$(window).scroll(function(){
+    if($(window).scrollTop() > 450){
+        $("#bobbie").animate({right: 1000});
+    }
+});
