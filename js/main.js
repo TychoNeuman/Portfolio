ac08d6452d82542facda27d8bd7c3e5de137a69a
TychoNeuman <tycho.neuman@mijngeldzaken.nl>
2019-12-10 19:32:03 +0100

    getting somewhere

--- js/main.js
+++ js/main.js
@@ -5,21 +5,21 @@ $(document).ready(function(){
     $('#bobbie').removeClass('hidden');
   }, 1200);
 });
-// Animate the title test
-$('#title').text('Front-end')
-  .delay(800)
-  .slideDown(800)
-  .slideUp(800, function(){
-    $('#title').text('Back-end')
-    .delay(800)
-    .slideDown(800)
-    .delay(800)
-    .slideUp(800);
-  });
+// // Animate the title test
+// $('#title').text('Front-end')
+//   .delay(800)
+//   .slideDown(800)
+//   .slideUp(800, function(){
+//     $('#title').text('Back-end')
+//     .delay(800)
+//     .slideDown(800)
+//     .delay(800)
+//     .slideUp(800);
+//   });
 
-// Animate Bobbie on scroll
-$(window).scroll(function(){
-    if($(window).scrollTop() > 450){
-        $("#bobbie").animate({right: 1000});
-    }
-});
+// // Animate Bobbie on scroll
+// $(window).scroll(function(){
+//     if($(window).scrollTop() > 450){
+//         $("#bobbie").animate({right: 1000});
+//     }
+// });
