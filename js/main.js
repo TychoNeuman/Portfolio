b870128ee07914ca0294cac296437073c4e4add2
TychoNeuman <tycho.neuman@mijngeldzaken.nl>
2019-12-12 20:36:24 +0100

    nav improvements

--- js/main.js
+++ js/main.js
@@ -1,32 +1,61 @@
-//Let's set the nav listeners
-// TODO : See if this can be refactored with a loop
+/**
+ * I should probably explain whats going on here. The function of this code is to
+ * nicely switch between pages on my site. It loops through a couple of objects
+ * that hold relevant info, and determines what page needs to be slideUp() or
+ * slideDown() respectivly. I wrote this while being very tired and it's 
+ * confusing to look at it now, so just don't touch it and everything should be fine.
+**/
 
-$('#nav-about-me').click(function(ev){
-  ev.preventDefault();
-  $('.slider-landing').slideUp('slow');
-  $('.slider-about').slideDown('slow');
-  $('footer').show();
+// $l_eHomeElement = $('.slider-landing');
+// $l_eAboutElement = $('.slider-about');
+// $l_ePortfolioElement = $('.slider-portfolio');
+// $l_eSkillsElement = $('.slider-skills');
+// $l_eContactElement = $('.slider-contact');
+
+l_aElementObjects = [
+  {
+    'sliderElement' : $('.slider-landing'),
+    'navElement' : $('#nav-home'),
+    'currentlyActive' : true,
+    'id' : 'nav-home'
+  },
+  {
+    'sliderElement' : $('.slider-about'),
+    'navElement' : $('#nav-about-me'),
+    'currentlyActive' : false,
+    'id' : 'nav-about-me'
+  },
+  {
+    'sliderElement' : $('.slider-portfolio'),
+    'navElement' : $('#nav-portfolio'),
+    'currentlyActive' : false,
+    'id' : 'nav-portfolio'
+  },
+  {
+    'sliderElement' : $('.slider-skills'),
+    'navElement' : $('#nav-skills'),
+    'currentlyActive' : false,
+    'id' : 'nav-skills'
+  },
+  {
+    'sliderElement' : $('.slider-contact'),
+    'navElement' : $('#nav-contact'),
+    'currentlyActive' : false,
+    'id' : 'nav-contact'
+  }
+]
+
+l_aElementObjects.forEach((element) => {         
+  element.navElement.click((clickedElement) => {
+    l_aElementObjects.forEach(element => {     
+      if(element.currentlyActive === true){
+        element.sliderElement.slideUp('slow');
+        element.currentlyActive = false;
+      }
+      if(element.id === clickedElement.currentTarget.id){
+        element.sliderElement.slideDown('slow');
+        element.currentlyActive = true;
+      }
+    });
+  });
 });
-
-$('#nav-portfolio').click(function(ev){
-  ev.preventDefault();
-  $('.slider-landing').slideUp('slow');
-  $('.slider-portfolio').slideDown('slow');
-  $('footer').show();
-});
-
-$('#nav-skills').click(function(ev){
-  ev.preventDefault();
-  $('.slider-landing').slideUp('slow');
-  $('.slider-skills').slideDown('slow');
-  $('footer').show();
-});
-
-$('#nav-contact').click(function(ev){
-  ev.preventDefault();
-  $('.slider-landing').slideUp('slow');
-  $('.slider-contact').slideDown('slow');
-  $('footer').show();
-});
-
-//nav-skills
